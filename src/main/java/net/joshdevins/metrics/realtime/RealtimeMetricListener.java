7b2487296927dbbb6c4b67fbb28643672eaf68e5
Josh Devins <josh@soundcloud.com>
2012-06-27 16:13:53 +0200

    Cleanup after fork of metrics.

--- src/main/java/net/joshdevins/metrics/realtime/RealtimeMetricListener.java
+++
@@ -1,9 +0,0 @@
-package net.joshdevins.metrics.realtime;
-
-import com.yammer.metrics.core.Metric;
-
-public interface RealtimeMetricListener<T extends Metric> {
-
-	public Class<T> getMetricType();
-
-}
